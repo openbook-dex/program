3436aac93fb1c1701aadaeabe073a46f6e33ad78
dd <dafydd.durairaj@gmail.com>
2021-02-16 10:22:47 -0500

    changed serum and msrm ids

--- dex/src/instruction.rs
+++ dex/src/instruction.rs
@@ -20,12 +20,12 @@ use proptest_derive::Arbitrary;
 
 pub mod srm_token {
     use solana_program::declare_id;
-    declare_id!("SRMuApVNdxXokk5GT7XD5cUUgXMBCoAz2LHeuAoKWRt");
+    declare_id!("2i6G2zTaJNeuuJUjrFAvdP7xvquDZREubRUWNwB3xsKQ");
 }
 
 pub mod msrm_token {
     use solana_program::declare_id;
-    declare_id!("MSRMcoVyrFxnSgo5uXwone5SKcGhT1KEJMFEkMEWf9L");
+    declare_id!("ZbVY71sQNng1uFNvgimLfWXeWyasDvou9bEcqexzYaT");
 }
 
 pub mod disable_authority {
